2f50a014e6a5f41f0e8072de890f22694659daa9
tobyhhw <1505994122@qq.com>
2024-03-05 16:08:39 +0800

    first commit

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    ref="_ref"
+    class="tb-button"
+    :class="{
+      [`tb-button--${type}`]: type,
+      [`tb-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import type { ButtonProps } from "./types";
+defineOptions({
+  name: "TbButton",
+});
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: "button",
+});
+
+const _ref = ref<HTMLButtonElement>();
+defineExpose({
+  ref: _ref,
+});
+</script>
+
+<style scoped></style>
